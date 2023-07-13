ae979a221a3fc94d1b5ef30585d5ebba38788ef1
Salih Imran Buker <sbuker@k1m35s06.42istanbul.com.tr>
2023-07-13 20:14:50 +0300

    ft_strnstr done

---
+++ ft_strnstr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbuker <sbuker@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 19:27:52 by sbuker            #+#    #+#             */
+/*   Updated: 2023/07/13 20:14:17 by sbuker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strnstr(const char *hs, const char *nd, size_t len){
+
+    int i;
+    int j;
+    int k;
+    int hs_len;
+    int nd_len;
+
+    i = 0;
+    j = 0;
+    k = 0;
+    hs_len = (int)len;
+    nd_len = strlen(nd);
+    
+    if(hs_len < nd_len)
+        return (NULL);
+
+    while(*(hs + i) != '\0' && *(nd + j) != '\0'){
+        if(*((const char *)hs + i) == *((const char *)nd + j))
+            j++;
+        if(j == 0)
+            k++;
+        i++;
+    }
+
+    if(j == nd_len)
+        return ((char *) hs + k);
+
+    return (NULL);
+}
--- makefile
+++ makefile
@@ -6,7 +6,7 @@
 #    By: sbuker <sbuker@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/12 17:21:29 by sbuker            #+#    #+#              #
-#    Updated: 2023/07/13 19:04:51 by sbuker           ###   ########.fr        #
+#    Updated: 2023/07/13 19:30:09 by sbuker           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@ NAME = libft.a
 SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 	ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c \
 	ft_strlcpy.c ft_strlcat.c ft_toupper.c ft_tolower.c ft_strchr.c \
-	ft_strrchr.c ft_strncmp.c ft_memchr.c ft_memcmp.c
+	ft_strrchr.c ft_strncmp.c ft_memchr.c ft_memcmp.c ft_strnstr.c
 
 OBJS = $(SRCS:.c=.o)
 
