d6bb59c50976ecad499d367cb41b23ffde820b17
Salih Imran Buker <sbuker@k1m30s09.42istanbul.com.tr>
2023-07-15 22:09:09 +0300

    ft_substr changed

--- ft_substr.c
+++ ft_substr.c
@@ -6,26 +6,26 @@
 /*   By: sbuker <sbuker@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 20:00:55 by sbuker            #+#    #+#             */
-/*   Updated: 2023/07/15 21:10:25 by sbuker           ###   ########.fr       */
+/*   Updated: 2023/07/15 22:08:11 by sbuker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char *ft_substr(char const *s, unsigned int start, size_t len){
-    unsigned int i;
-    char *sub;
-
-    i = 0;
-    sub = (char *)ft_calloc(len, 1);
-
-    while((char *)s && (char *)sub != NULL){
-        if(start == i){
-            ft_strncpy((char *)sub, (const char *)s + i, len);
-            break;
-        }
-        i++;
+    if (start >= ft_strlen(s)) {
+        return NULL;
     }
-    
-    return ((char *)sub);
+
+    char* substr = (char*)malloc((len + 1) * sizeof(char));  
+
+    if (substr == NULL) {
+        return NULL;  
+    }
+
+    ft_strncpy(substr, s + start, len);  
+
+    substr[len] = '\0'; 
+
+    return substr;
 }
