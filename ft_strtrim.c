2cd015a8dc62a0190c118d2db47eb1c9326a0241
sbuker <sbuker@student.42istanbul.com.tr>
2023-07-16 20:44:57 +0300

    ft_strtrim done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,13 +5,48 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbuker <sbuker@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/16 04:27:56 by sbuker            #+#    #+#             */
-/*   Updated: 2023/07/16 04:28:23 by sbuker           ###   ########.fr       */
+/*   Created: 2023/07/16 18:55:41 by sbuker            #+#    #+#             */
+/*   Updated: 2023/07/16 20:44:06 by sbuker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set){
-    
+int is_in_set(char c, char const *set) {
+    while (*set) {
+        if (c == *set)
+            return 1;
+        set++;
+    }
+    return 0;
+}
+
+char *ft_strtrim(char const *s1, char const *set) {
+    if (s1 == NULL || set == NULL)
+        return NULL;
+
+    char const *start = s1;
+    char const *end = s1;
+
+    while (is_in_set(*start, set))
+        start++;
+
+    while (*end)
+        end++;
+
+    while (end > start && is_in_set(*(end - 1), set))
+        end--;
+
+    int len = end - start;
+    char *trimmed = (char *)malloc((len + 1) * sizeof(char));
+    if (trimmed == NULL)
+        return NULL;
+
+    char *ptr = trimmed;
+    while (len--)
+        *ptr++ = *start++;
+
+    *ptr = '\0';
+
+    return trimmed;
 }
